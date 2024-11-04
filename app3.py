7a192363e5606b3ee3692795d1e6df54a8afb673
Matheus Alcantara Pereira <167790351+matheus-alcan@users.noreply.github.com>
2024-11-04 08:28:13 -0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,5 +1,5 @@
 def adicionar_contato(agenda):
-    nome = input("Digite o nome do contato ")
+    nome = input("Digite o nome do contato: ")
     telefone = input("Digite o telefone do contato: ")
     email = input("Digite o email do contato: ")
     contato = {"nome": nome, "telefone": telefone, "email": email}
